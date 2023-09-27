3e439645031432cc92de6145bbe361d061047fdc
otompo <abagnasalifusasco@gmail.com>
2023-09-27 23:47:40 +0000

    Latest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import Home from "./pages/Home";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import About from "./pages/About";
-import SignUp from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 
